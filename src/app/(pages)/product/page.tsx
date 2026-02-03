398924f9cb649b3d79c353fad1177174bfc1c648
Youssef Turkey <youssef.turkey11@gmail.com>
2026-02-04 01:12:48 +0200

    protected routes

---
+++ src/app/(pages)/product/page.tsx
@@ -0,0 +1,5 @@
+const Product = () => {
+  return <section className="sec">Product</section>;
+};
+
+export default Product;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,11 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-poppins)", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
